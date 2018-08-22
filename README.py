5299ba9c6e3b644d98f697ab62272bd18391ddc1
Shantanu Oak <shantanu.oak@gmail.com>
2018-08-22 14:09:12 +0000

    Update README.py

--- README.py
+++ README.py
@@ -1,13 +1,17 @@
 # easystemmer
 #Stemmer for Indian Names
 
-
-
 from easystemmer import IndianNameStemmer
+import pandas as pd
+
 s = IndianNameStemmer()
-s.stem(['savithabai', 'aabaaaaa'])
+#s.stem(['savithabai'])
 
-#It will return the stemmed version of the names like...
-('saveta', 'aba')
+df=pd.read_excel('my_file.xlsx')
+df.columns=['wrong', 'correct']
 
+df['updated']=df['wrong'].astype(str).apply(lambda x: x.split()).apply(s.stem)
+df['updated1']=df['correct'].astype(str).apply(lambda x: x.split()).apply(s.stem)
 
+ndf=df[df['updated'] != df['updated1']]
+ndf[['wrong', 'correct']].to_excel('to_study.xlsx')
