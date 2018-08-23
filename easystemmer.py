33d265cfba547a7693df60d92337ae1218583be7
Shantanu Oak <shantanu.oak@gmail.com>
2018-08-23 06:28:48 +0000

    Update easystemmer.py

--- easystemmer.py
+++ easystemmer.py
@@ -9,9 +9,8 @@ class IndianNameStemmer(StemmerI):
             i = re.sub(r'(\w)\1+',r'\1', i)
             i = i[:-2] if i.endswith(('bi', 'ji', 'so')) else i            
             i = i[:-3] if i.endswith(('bai', 'ben', 'beg', 'tai', 'sab', 'rao', 'rav', 'sha', 'kha', 'lal')) else i
-            i = i[:-4] if i.endswith(('bhai', 'sing')) else i
+            i = i[:-4] if i.endswith(('bhai', 'sing', 'devi')) else i
             i = i[:-5] if i.endswith(('saheb', 'singh' , 'shing')) else i
-            print (i)
             for r in (("tha", "ta"), ("i", "e"), ("gi", "ji"), ("bh", "b"), ("v", "w"), ("yu", "u"), ("gh", "g"), 
                       ("dh", "d"), ("sh", "s"), ("bh","b"), ("ph","f"), ("z","j"), ("gaw", "gaon"), ("gav", "gaon"), ("a","") ):
                 i = i.replace(*r)
