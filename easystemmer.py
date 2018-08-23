da0a61d08bb42d8f1bb8da3943bcf82c83fc2486
Shantanu Oak <shantanu.oak@gmail.com>
2018-08-23 02:29:17 +0000

    Update easystemmer.py

--- easystemmer.py
+++ easystemmer.py
@@ -5,6 +5,7 @@ class IndianNameStemmer(StemmerI):
     def stem(self, token):
         newtup=list()
         for i in token:
+            i = re.sub(r'(\w)\1+',r'\1', i)
             i = i[:-1]+'v' if i.endswith('o') else i
             i = i[:-2] if i.endswith(('bi', 'ji', 'so')) else i            
             i = i[:-3] if i.endswith(('bai', 'ben', 'beg', 'tai', 'sab', 'rao', 'rav', 'sha', 'kha', 'lal')) else i
@@ -12,8 +13,8 @@ class IndianNameStemmer(StemmerI):
             i = i[:-5] if i.endswith(('saheb', 'singh' , 'shing')) else i
      
             for r in (("tha", "ta"), ("i", "e"), ("gi", "ji"), ("bh", "b"), ("v", "w"), ("yu", "u"), ("gh", "g"), 
-                      ("dh", "d"), ("sh", "s"), ("gaw", "gaon"), ("gav", "gaon") ):
+                      ("dh", "d"), ("sh", "s"), ("bh","b"), ("ph","f"), ("z","j"), ("gaw", "gaon"), ("gav", "gaon") ):
                 i = i.replace(*r)
-                i = re.sub(r'(\w)\1+',r'\1', i)
+
             newtup.append(''.join(i for i, _ in itertools.groupby(i)))
         return tuple(newtup)
