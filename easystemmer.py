47ca81207c07085737b5b95339db271499701157
Shantanu Oak <shantanu.oak@gmail.com>
2018-08-23 02:57:57 +0000

    Update easystemmer.py

--- easystemmer.py
+++ easystemmer.py
@@ -9,11 +9,11 @@ class IndianNameStemmer(StemmerI):
             i = i[:-1]+'v' if i.endswith('o') else i
             i = i[:-2] if i.endswith(('bi', 'ji', 'so')) else i            
             i = i[:-3] if i.endswith(('bai', 'ben', 'beg', 'tai', 'sab', 'rao', 'rav', 'sha', 'kha', 'lal')) else i
-            i = i[:-4] if i.endswith(('bhai', 'sing')) else i
+            i = i[:-4] if i.endswith(('bhai', 'sing', 'devi')) else i
             i = i[:-5] if i.endswith(('saheb', 'singh' , 'shing')) else i
      
             for r in (("tha", "ta"), ("i", "e"), ("gi", "ji"), ("bh", "b"), ("v", "w"), ("yu", "u"), ("gh", "g"), 
-                      ("dh", "d"), ("sh", "s"), ("bh","b"), ("ph","f"), ("z","j"), ("gaw", "gaon"), ("gav", "gaon") ):
+                      ("dh", "d"), ("sh", "s"), ("bh","b"), ("ph","f"), ("z","j"), ("gaw", "gaon"), ("gav", "gaon"), ("a", "") ):
                 i = i.replace(*r)
 
             newtup.append(''.join(i for i, _ in itertools.groupby(i)))
