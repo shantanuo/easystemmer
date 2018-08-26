71ad47892d523aeac7a414dfba1d22ceefee63f4
Shantanu Oak <shantanu.oak@gmail.com>
2018-08-26 12:32:39 +0000

    Update easystemmer.py

--- easystemmer.py
+++ easystemmer.py
@@ -2,16 +2,21 @@ class IndianNameStemmer(StemmerI):
     def stem(self, token):
         newtup=list()
         for i in token:
-            i = re.sub(r'(^\w{2,3}\. ?)', r'', i)
+            i = re.sub(r'\([^)]*\)', '', i).strip()
+            i = re.sub(r'\[[^)]*\]', '', i).strip()
+            i = re.sub(r'(^\w{1,3}\. ?)', r'', i)
+            i = i.split('.')[0]
+            i = i.split('-')[0]
             i = re.sub(r'[^a-zA-Z]', '', i)
             i = re.sub(r'(\w)\1+',r'\1', i)
             i = i[:-2] if i.endswith(('bi', 'ji', 'so')) else i            
             i = i[:-3] if i.endswith(('bai', 'ben', 'beg', 'tai', 'sab', 'rao', 'rav', 'kha', 'lal', 'dev', 'deo')) else i
             i = i[:-4] if i.endswith(('bhai', 'bhau', 'sing', 'devi')) else i
             i = i[:-5] if i.endswith(('saheb', 'singh' , 'shing', 'kumar')) else i
-            for r in (("tha", "ta"), ("gi", "ji"), ("bh", "b"), ("v", "w"), ("yu", "u"), ("gh", "g"),  
-                      ("bh","b"), ("ph","f"),  ("gaw", "gaon"), ("ksh", "x"), ("z","j"), ("y", "i"), 
-                      ("dh", "d"), ("sh", "s"), ("g","j"), ("i", "e"), ("a","") ):
+            for r in (("tha", "ta"), ("krish", "krush"), ("gi", "ji"), ("bh", "b"),  ("yu", "u"), ("gh", "g"),  
+                      ("bh","b"), ("ph","f"),  ("gaw", "gaon"), ("ksh", "x"), 
+                      ("dh", "d"), ("sh", "s") ):
+                # ("g","j"), ("o", "u"), ("i", "e"), ("m", "n"),  ("y", "i"), ("v", "w"), ("z","j"),, ("a","")
                 i = i.replace(*r)
 
             newtup.append(''.join(i for i, _ in itertools.groupby(i)))
